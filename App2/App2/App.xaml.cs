8831a99aa0fee7304ffcda11b0b9caa859bbf3fb
Guilherme Camilo Rezende <gcamilor@outlook.com>
2017-05-09 22:05:01 -0300

    Creado projeto bugado

---
+++ App2/App2/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new App2.MainPage();
+        }
+
+        protected async override void OnStart()
+        {
+            Class1 c1 = new Class1();
+            var text = await c1.GetMy();
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ App2/App2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using ModernHttpClient;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class Class1
+    {
+        public async Task<string> GetMy()
+        {
+            try
+            {
+                HttpClient client = new HttpClient(new NativeMessageHandler());
+                client.BaseAddress = new Uri("https://randomuser.me/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.GetAsync("api/");
+                string content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                var er = ex;
+                return er.ToString();
+            }
+        }
+    }
+}
